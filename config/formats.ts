da47bad9b73893b239026cdaa004c8bf81d79b36
EverOddish <everoddish@gmail.com>
2021-03-22 18:36:30 -0400

    Plat2Fantina - Banend Rotom forms

--- config/formats.ts
+++ config/formats.ts
@@ -2995,7 +2995,7 @@ export const Formats: FormatList = [
 		mod: 'gen4',
         maxLevel: 26,
 		ruleset: ['Standard'],
-        banlist: ['Alakazam', 'Steelix', 'Gengar', 'Dragon Rage'],
+        banlist: ['Alakazam', 'Steelix', 'Gengar', 'Dragon Rage', 'Rotom-Fan', 'Rotom-Frost', 'Rotom-Heat', 'Rotom-Mow', 'Rotom-Wash'],
 		onValidateTeam(team) {
             let data = {
                 TwinleafTown: [['Magikarp', 3], ['Gyarados', 3]],
